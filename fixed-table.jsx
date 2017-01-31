3234c9ca201c647b5e297c558e46712e533fa0e5
Yury Orlov <mpcteam.kvet@gmail.com>
2017-01-31 11:54:34 +0300

    Update fixed table example

--- fixed-table.jsx
+++ fixed-table.jsx
@@ -21,33 +21,83 @@ let rows = (() => {
     return rows;
 })();
 
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.columnTitles = {
+            id: 'ID',
+            title: 'Title',
+            count: 'count'
+        };
+
+        this.columnWidths = {
+            id: 50,
+            title: 200,
+            count: 200
+        };
+
+        this.state = {
+            columnOrder: ['collapse'].concat(Object.keys(this.columnTitles)),
+            collapsedRows: new Set()
+        };
+
+        this.toggleRow = this.toggleRow.bind(this);
+        this.hideColumn = this.hideColumn.bind(this);
+    }
+    
+    toggleRow(rowIndex) {
+        let collapsedRows = this.state.collapsedRows;
+        collapsedRows.has(rowIndex) ? collapsedRows.delete(rowIndex) : collapsedRows.add(rowIndex);
+        this.setState({
+            collapsedRows: collapsedRows
+        })
+    }
+    
+    hideColumn(columnKey) {
+        let columnOrder = this.state.columnOrder;
+        columnOrder.splice(columnOrder.indexOf(columnKey), 1)
+        this.setState({
+            columnOrder: columnOrder
+        })
+    }
+
+    render() {
+        return (
+            <Table
+                rowHeight={50}
+                rowsCount={rows.length}
+                rowHeightGetter={(index) => this.state.collapsedRows.has(index) ? 100 : 30}
+                height={500}
+                width={1000}
+                headerHeight={50}>
+                {this.state.columnOrder.map((columnKey, i) => {
+                    return <Column
+                        columnKey={columnKey}
+                        key={i}
+                        header={
+                            <Cell>
+                                {this.columnTitles[columnKey]}
+                                {columnKey !== 'collapse' && <span style={{ marginLeft: '10px' }} onClick={() => this.hideColumn(columnKey)}>x</span>}
+                            </Cell>}
+                        cell={(e) => {
+                            return columnKey === 'collapse'
+                                ? <Cell onClick={() => this.toggleRow(e.rowIndex)}>></Cell>
+                                : <Cell>{rows[e.rowIndex][e.columnKey]}</Cell>
+                            }
+                        }
+                        align={columnKey === 'id' ? 'right' : 'left'}
+                        fixed={i === 0}
+                        width={this.columnWidths[columnKey] || 30}
+                    />;
+                })}
+            </Table>
+        );
+    }
+};
+
 // Render your table
 ReactDOM.render(
-  <Table
-    rowHeight={50}
-    rowsCount={rows.length}
-    height={500}
-    width={1000}
-    headerHeight={50}>
-    <Column
-      columnKey="id"
-      align="right"
-      header={<Cell>ID</Cell>}
-      cell={(e) => <Cell>{rows[e.rowIndex][e.columnKey]}</Cell>}
-      width={200}
-    />
-    <Column
-      columnKey="title"
-      header={<Cell>Title</Cell>}
-      cell={(e) => <Cell>{rows[e.rowIndex][e.columnKey]}</Cell>}
-      width={100}
-    />
-    <Column
-      columnKey="count"
-      header={<Cell>Count</Cell>}
-      cell={(e) => <Cell>{rows[e.rowIndex][e.columnKey]}</Cell>}
-      width={200}
-    />
-  </Table>,
+  <App/>,
   document.getElementById('app')
 );
